1440403715075c24f1f6d58c0b43b513e6c91039
User <User@YD02-12>
2020-01-21 12:37:39 +0900

    hello

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+   /*
+    * ctrl+/ 주석 단축키
+    * ctrl+d 라인삭제
+    * 여러라인 주석처리할때 사용 /**'
+    * 작성자 : 이다혜 2020.01.21
+    */
+   public static void main (String[] args) {
+      // Hellow World 출력구문
+	   int age = 10;
+	   int year = 2020;
+	   System.out.println("올해는"+year+"입니다");
+      System.out.println("내 나이는" + age + "입니다");
+//      System.out.println("Hello, World"); 
+   }
+}
