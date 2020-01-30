5ccb524b2743e9306d10c9007a3d712f9104e32c
User <User@YD02-12>
2020-01-30 17:51:20 +0900

    0130

---
+++ Hello World/src/classes/peopleExample.java
@@ -0,0 +1,29 @@
+package classes;
+
+public class peopleExample {
+	public static void main(String[] args) {
+		People pe = new People();
+		System.out.println(pe.name);
+		System.out.println(pe.age);
+		System.out.println(pe.height);
+		System.out.println(pe.weight);
+
+		System.out.println("=======================");
+
+		People peo = new People();
+		peo.name = "kim";
+		peo.age = 27;
+		peo.height = 160;
+		peo.weight = 50;
+
+		System.out.println(peo.name);
+		System.out.println(peo.age);
+		System.out.println(peo.height);
+		System.out.println(peo.weight);
+
+		peo.study();
+		peo.hobby();
+		System.out.println(peo.getAge());
+
+	}
+}
--- Hello World/src/referenceType/Exercise09Chapter5.java
+++ Hello World/src/referenceType/Exercise09Chapter5.java
@@ -1,63 +1,50 @@
-package referenceType;
+  package referenceType;
 
 import java.util.Scanner;
 
 public class Exercise09Chapter5 {
-//******************************************
-	
 	public static void main(String[] args) {
 		boolean run = true;
 		int studentNum = 0;
 		int[] scores = null;
-		Scanner scanner = new Scanner(System.in);
-		
-		while(run) {
-			System.out.println("----------------------------------------------");
-			System.out.println("1.학생수 | 2.점수입력 | 3.점수리스트 | 4.분석| 5.종료");
-			System.out.println("----------------------------------------------");
-			System.out.print("선택> ");
-			
-			int selectNo = scanner.nextInt();
-			
-			if(selectNo == 1 ) {
-				System.out.println("학생수> ");					
-				studentNum = scanner.nextInt();
+		Scanner scn = new Scanner(System.in);
+
+		while (run) {
+			System.out.println("-------------------------");
+			System.out.println("1.학생수|2.점수입력|3.점수리스트|4.분석|5.종료");
+			System.out.println("-------------------------");
+			System.out.print("선택>");
+			int selectNo = scn.nextInt();
+			if (selectNo == 1) {
+				System.out.print("학생수>");
+				studentNum = scn.nextInt();
 				scores = new int[studentNum];
-				
-			}else if(selectNo == 2) {
-				for(int i=0; i<scores.length; i++) {
-				   System.out.print("scores[ " +i+ "]>");
-				   int score = scanner.nextInt();
-				   scores[i] = score;
-		
-				
-				
-			}else if(selectNo == 3) {
-				for (int i = 0; i)
-			
-				System.out.println("점수리스트> ");
-				int inputSd = scanner.nextInt();
-				System.out.println("리스트>" + scores);
-				
-				
-			}else if(selectNo == 4) {
-				
+			} else if (selectNo == 2) {
+				for (int i = 0; i < scores.length; i++) {
+					System.out.print("scores[" + i + "]>");
+					int score = scn.nextInt();
+					scores[i] = score;
+				}
+			} else if (selectNo == 3) {
+				for (int i = 0; i < scores.length; i++) {
+					System.out.print("scores[" + i + "]>: " + scores[i]);
+				}
+			} else if (selectNo == 4) {
 				int max = 0;
-				sum =0, cnt =0;
-				
-			}
-				System.out.println("분석> ");
-				
-				
-			}else if (selectNo== 5) {
+				int sum = 0, cnt = 0;
+				for (int i = 0; i < scores.length; i++) {
+					sum += scores[i];
+					cnt++;
+					if (max < scores[i]) {
+						max = scores[i];
+					}
+				}
+				System.out.println("최고점수: " + max);
+				System.out.println("평균점수: " + ((double) sum / cnt));
+			} else if (selectNo == 5) {
 				run = false;
-				
-		}	
-		}
-		
+			}
+		} // end of while
 		System.out.println("프로그램 종료.");
-   }
-	
-	
-//*****************************************
+	}
 }
